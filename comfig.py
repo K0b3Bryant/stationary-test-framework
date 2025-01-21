673498082d62537740065785457b5578da19c495
Marco <sejereje1011@gmail.com>
2025-01-21 11:11:50 +0100

    Update comfig.py

--- comfig.py
+++ comfig.py
@@ -1,5 +1,9 @@
 CONFIG = {
     'date_format': '%Y-%m-%d',
     'p_value_threshold': 0.05,
-    'return_type': 'log'  # Options: 'log' or 'simple'
+    'enable_kpss_test': True,
+    'enable_rolling_test': True,
+    'rolling_window_size': 3,
+    'adf_max_lag': None,  # None lets the function automatically determine the max lag
+    'kpss_regression': 'c'  # Options: 'c' (constant), 'ct' (constant + trend)
 }
